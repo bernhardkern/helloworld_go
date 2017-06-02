b8e31b6dc3f808b91e544bd21185f274154858da
Johannes Brüderl <jbruederl@it-economics.de>
2017-06-02 16:48:44 +0200

    remove debugging/testing stuff from main

--- main.go
+++ main.go
@@ -4,17 +4,15 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/julienschmidt/httprouter"
 	"log"
+
 	"github.com/bernhardkern/helloworld_go/data"
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/bernhardkern/helloworld_go/rest"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
-	fmt.Println("Hello world")
-	fmt.Println("Hello world3")
-
 	mySQLRepo, err := data.NewMySQL()
 	if err != nil {
 		fmt.Println("bad error", err)
@@ -24,18 +22,6 @@ func main() {
 
 	router.GET("/person", restHandler.HandleGet)
 	router.POST("/person", restHandler.HandlePost)
-
-	person := data.Person{Id: 1, FirstName: "B", LastName: "K"}
-	person2 := data.Person{Id: 2, FirstName: "Johannes", LastName: "Brüderl"}
-	err = mySQLRepo.Create(person)
-	if err != nil {
-		fmt.Println("bad error", err)
-	}
-	err = mySQLRepo.Create(person2)
-	if err != nil {
-		fmt.Println("bad error", err)
-	}
-	fmt.Println("finished successfully")
 
 	log.Fatal(http.ListenAndServe(":8888", router))
 }
